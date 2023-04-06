1003cf97ef30481c162f9338861a26be5b96e994
Coffee58511867 <marobahlalele910@gmail.com>
2023-04-06 11:04:08 +0200

    Added Auth Services

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+
+    public afStore: AngularFirestore,
+    public ngFireAuth: AngularFireAuth,
+    public router: Router,
+  ) { }
+
+    // Login in with email/password
+    SignIn(email: string, password: string) {
+      return this.ngFireAuth.signInWithEmailAndPassword(email, password);
+    }
+    // Register user with email/password
+    RegisterUser(email: string, password: string) {
+      return this.ngFireAuth.createUserWithEmailAndPassword(email, password);
+    }
+    // Email verification when new user register
+    SendVerificationMail() {
+      return this.ngFireAuth.currentUser.then((user: any) => {
+        return user.sendEmailVerification().then(() => {
+          this.router.navigate(['home']);
+        });
+      });
+    }
+      // Recover password
+  PasswordRecover(passwordResetEmail: string) {
+    return this.ngFireAuth
+      .sendPasswordResetEmail(passwordResetEmail)
+      .then(() => {
+        window.alert(
+          'Password reset email has been sent, please check your inbox.'
+        );
+      })
+      .catch((error) => {
+        window.alert(error);
+      });
+  }
+  SignOut() {
+    return this.ngFireAuth.signOut().then(() => {
+      this.router.navigate(['home']);
+    });
+  }
 }
