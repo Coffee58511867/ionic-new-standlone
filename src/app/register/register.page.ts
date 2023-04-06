34f567a3ce433919fbeb0f1024bbbf6b93590e98
Coffee58511867 <marobahlalele910@gmail.com>
2023-04-06 11:11:46 +0200

    Added Register

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,20 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule, RouterModule]
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  Register! : FormGroup;
+
+  constructor(public fireAuth : AngularFireAuth,
+    public formBuilder:  FormBuilder,
+    public router: Router
+    ) { }
 
   ngOnInit() {
+    this.Register = this.formBuilder.group({
+      names : ['', Validators.required],
+      location : ['', Validators.required],
+      email : ['', [Validators.required, Validators.email]],
+      phoneNumber : ['', [Validators.required, Validators.minLength(8)]],
+      password : ['', [Validators.required, Validators.minLength(8)]],
+    })
+  }
+
+  register(email: any, password: any){
+   this.fireAuth.createUserWithEmailAndPassword(email,password)
+   .then((res)=> {
+    console.log(res)
+    this.router.navigate(['/login']);
+
+   })
+   .catch((err)=> {
+    console.log(err);
+    window.alert(err);
+   })
   }
 
 }
